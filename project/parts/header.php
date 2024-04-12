649e6b007ea0f07db0a11e1892233c1032fc34a1
NikTikRik <161849908+NikTikRik@users.noreply.github.com>
2024-04-12 14:47:28 +0200

    Add files via upload

---
+++ project/parts/header.php
@@ -0,0 +1,23 @@
+<header class="header-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <a href="index.php" class="logo">
+                        <h1>Villa</h1>
+                    </a>
+                    <ul class="nav">
+                        <li><a href="index.php" class="active">Home</a></li>
+                        <li><a href="properties.html">Properties</a></li>
+                        <li><a href="property-details.html">Property Details</a></li>
+                        <li><a href="contact.html">Contact Us</a></li>
+                        <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
+                    </ul>   
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
