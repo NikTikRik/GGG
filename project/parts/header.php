7e55cd88bbc27cd6e35e3e547f323c54b8e0b462
NikTikRik <161849908+NikTikRik@users.noreply.github.com>
2024-04-12 15:15:07 +0200

    Add files via upload

--- project/parts/header.php
+++ project/parts/header.php
@@ -1,4 +1,4 @@
-<header class="header-area">
+<header class="header-area header-sticky">
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -8,9 +8,9 @@
                     </a>
                     <ul class="nav">
                         <li><a href="index.php" class="active">Home</a></li>
-                        <li><a href="properties.html">Properties</a></li>
-                        <li><a href="property-details.html">Property Details</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
+                        <li><a href="properties.php">Properties</a></li>
+                        <li><a href="property-details.php">Property Details</a></li>
+                        <li><a href="contact.php">Contact Us</a></li>
                         <li><a href="#"><i class="fa fa-calendar"></i> Schedule a visit</a></li>
                     </ul>   
                     <a class='menu-trigger'>
